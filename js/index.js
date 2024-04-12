9ffa19d166f471b112e2fffd8f713f47a8284489
JohanFlotten <johanflotten@gmail.com>
2024-04-13 00:58:42 +0200

    done for now ugh

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import movies from './module.js';
-// Get the card-container element
-const cardContainer = document.getElementById("card-container");
 
+// Get the card-container element
+const cardContainer = document.querySelector("#card-container");
 // Iterate over the movies array
 movies.forEach(movie => {
     // Create a new card element
@@ -9,6 +9,11 @@ movies.forEach(movie => {
     card.classList.add("card");
 
     card.style.backgroundImage = movie.backgroundImage;
+
+    const movieQuallity =document.createElement("h3")
+    movieQuallity.textContent = movie.movieQuallity;
+    movieQuallity.classList.add("movie-quallity")
+    card.classList.add("movie-quallity")
 
     // Create elements for movie title and rating
     const titleElement = document.createElement("h3");
@@ -20,9 +25,43 @@ movies.forEach(movie => {
     ratingElement.classList.add("movie-rating");
 
     // Append elements to the card
+    card.appendChild(movieQuallity);
     card.appendChild(titleElement);
     card.appendChild(ratingElement);
 
     // Append the card to the card-container
     cardContainer.appendChild(card);
+});
+
+import {latestMovies} from './module.js';
+
+const rightContainer =document.querySelector("#right-container");
+
+latestMovies.forEach(lMovies => {
+
+    const card = document.createElement("div")
+    card.classList.add("rcard")
+
+    card.style.backgroundImage = lMovies.backgroundImage;
+
+    const rcmovieQuallity =document.createElement("h3")
+    rcmovieQuallity.textContent = lMovies.movieQuallity;
+    rcmovieQuallity.classList.add("movie-quallity")
+    card.classList.add("movie-quallity")
+
+    const rctitleElement = document.createElement("h3")
+    rctitleElement.textContent = lMovies.title;
+    rctitleElement.classList.add("movie-title");
+    card.classList.add("movie-title")
+
+    const rcratingElement =document.createElement("p")
+    rcratingElement.textContent = `Rating: ${lMovies.rating}`;
+    rcratingElement.classList.add("movie-rating");
+    card.classList.add("movie-rating")
+
+    card.appendChild(rcmovieQuallity);
+    card.appendChild(rctitleElement);
+    card.appendChild(rcratingElement);
+
+    rightContainer.appendChild(card);
 });
